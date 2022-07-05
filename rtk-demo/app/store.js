41c481cfe306ed326afbf7c7e6d0e691bc550f6f
Durga Venkata Sai Maneeswar Mutyala <durga.mutyala@vedantu.com>
2022-07-05 16:07:54 +0530

    async actions using extra reducers

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -2,14 +2,14 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
-// const userReducer = require("../features/user/userSlice");
+const userReducer = require("../features/user/userSlice");
 const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
-    // user: userReducer,
+    user: userReducer,
   },
 //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
